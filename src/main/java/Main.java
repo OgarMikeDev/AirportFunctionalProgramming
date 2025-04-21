68d08f76225bc8d711b3f861efd4a5ed4ccc966b
Mike Ogar <ogar.m.v@mail.ru>
2025-04-21 18:02:33 +0300

    Airport

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,52 +30,21 @@ public class Main {
     public static long findCountAircraftWithModelAirbus(Airport airport, String model) {
         //TODO Метод должен вернуть количество самолетов указанной модели.
         // подходят те самолеты, у которых name начинается со строки model
-        return airport.getAllAircrafts().stream()
-                .map(Aircraft::getModel)
-                .filter(currentModel -> currentModel.contains(model))
-                .count();
     }
 
     public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
         //TODO Метод должен вернуть список отправляющихся рейсов в ближайшее количество часов.
-        long second = hours * 60 * 60;
-        Instant instantNow = Instant.now();
-        return airport.getTerminals().stream()
-                .map(Terminal::getFlights)
-                .flatMap(List::stream)
-                .filter(flight -> {
-                    return flight.getType().equals(Flight.Type.DEPARTURE)
-                            && flight.getDate().isAfter(instantNow)
-                            && flight.getDate().isBefore(instantNow.plusSeconds(second));
-                })
-                .collect(Collectors.toList());
+
     }
 
     public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
         //TODO Метод должен вернуть словарь с количеством припаркованных самолетов в каждом терминале.
-        return airport.getTerminals().stream()
-                .collect(Collectors.toMap(Terminal::getName,
-                        terminal -> terminal.getParkedAircrafts().size()));
+
     }
 
 
     public static Optional<Flight> findFirstFlightArriveToTerminal(Airport airport, String terminalName) {
         //TODO Найти ближайший прилет в указанный терминал.
-        Instant instantNow = Instant.now();
 
-        Terminal firstTerminal =
-                airport.getTerminals().stream()
-                        .filter(currentTerminal -> currentTerminal.getName().equals(terminalName))
-                        .findFirst()
-                        .orElse(null);
-
-        if (firstTerminal == null) {
-            return Optional.empty();
-        }
-
-        return firstTerminal.getFlights().stream()
-                .filter(currentFlight -> currentFlight.getType().equals(Flight.Type.ARRIVAL)
-                        && currentFlight.getDate().isAfter(instantNow))
-                .min(Comparator.comparing(flight -> flight.getDate()));
     }
 }
