dc74c2ef041ccc8ec2f0516c64530b57c3d1f02d
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-04-24 12:02:04 +0300

    Update Flight.java

--- src/main/java/library/Flight.java
+++ src/main/java/library/Flight.java
@@ -10,7 +10,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-public class Flight implements Comparable<Flight> {
+public class Flight {
     private static final ZoneId zoneId = ZoneId.systemDefault();
     private static final DateTimeFormatter HOUR_FORMAT;
     private final String code;
@@ -48,24 +48,6 @@ public class Flight implements Comparable<Flight> {
 
     static {
         HOUR_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm").withZone(zoneId);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Flight flight = (Flight) o;
-        return Objects.equals(code, flight.code) && type == flight.type && Objects.equals(date, flight.date) && Objects.equals(aircraft, flight.aircraft);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(code, type, date, aircraft);
-    }
-
-    @Override
-    public int compareTo(Flight currentFlight) {
-        return this.getDate().compareTo(currentFlight.getDate());
     }
 
     public static enum Type {
